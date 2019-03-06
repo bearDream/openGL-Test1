1751b8a769958a14e6b370be0b8606b13f9b02d0
laxzhang <laxzhang@outlook.com>
2019-03-06 15:08:55 +0800

    Initial Commit

---
+++ openGL-TEST1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL-TEST1
+//
+//  Created by Lax Zhang on 2019/3/6.
+//  Copyright © 2019 Lax Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
