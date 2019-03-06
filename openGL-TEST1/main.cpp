23fc04b153ae4608e7872cf155f6cf1a020a77d2
laxzhang <laxzhang@outlook.com>
2019-03-06 23:49:20 +0800

    sort of code

--- openGL-TEST1/main.cpp
+++ openGL-TEST1/main.cpp
@@ -28,6 +28,9 @@ void framebuffer_size_callback(GLFWwindow*, int, int);
 void processInput(GLFWwindow*);
 void mouseCallback(GLFWwindow*, double, double);
 void scrollCallback(GLFWwindow *, double, double);
+void setVertices();
+void setTextures();
+void calculateInLoop();
 void renderScene(Shader &shader);
 void renderLightSource(Shader &shader);
 void renderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
@@ -50,17 +53,18 @@ glm::vec3 heroBoxTrans = glm::vec3(1.0);
 FontsManager fontsManager;
 
 // timing
-float deltaTime =0.0f;
+float initial_time, deltaTime =0.0f;
 float lastTime = 0.0f;
 int frame = 0, frameCount = 0;
 
-GLuint Fl_VAO, cube_VAO, lighterVAO, sphereVAO, TextVAO, Text_VBO;
+GLuint Fl_VAO, Fl_VBO, cube_VAO, cube_VBO, lighterVAO, sphereVAO, sphereVBO, sphereEBO, TextVAO, Text_VBO;
 vector<GLuint> sphere_indices;
 GLuint floorTextureID, boxTextureID, sunTextureID, moonTextureID;
 
 // 光源位置
 glm::vec3 lightPos = glm::vec3(3.5f, 3.0f, 3.5f);
 
+// 素材地址
 char texture_floor[255] = "resources/images/wood.png";
 char texture_box[255] = "resources/images/container2.png";
 char texture_sun[255] = "resources/images/2k_sun.jpg";
@@ -68,129 +72,37 @@ char texture_moon[255] = "resources/images/2k_moon.jpg";
 char font_roman[255] = "resources/fonts/Times New Roman.ttf";
 
 int main(int argc, const char * argv[]) {
+    // 1. 初始化
     if(init() == -1){
         glfwTerminate();
         return 0;
     }
 
-    // 编译着色器
-    Shader shadowShader("shaders/shader_obj.vs", "shaders/shader_obj.fs");
+    // 2. 编译着色器
+    Shader objShader("shaders/shader_obj.vs", "shaders/shader_obj.fs");
     Shader lampShader("shaders/shader_lighter.vs", "shaders/shader_lighter.fs");
     Shader textShader("shaders/shader_fonts.vs", "shaders/shader_fonts.fs");
     
-    textShader.use();
-    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(window_width), 0.0f, static_cast<GLfloat>(window_height));
-    textShader.setMat4("projection", projection);
-    // 球体
-    Sphere sphere(0.5f, 60, 60);
-    vector<float> sphere_vertices = sphere.getVertices();
-    sphere_indices = sphere.getIndices();
+    // 3. 顶点/缓冲/索引设置
+    setVertices();
     
-    // =======立方体=======
-    unsigned int VBO;
-    glGenVertexArrays(1, &cube_VAO);
-    glGenBuffers(1, &VBO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindVertexArray(cube_VAO);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(6 * sizeof(float)));
-    
-    // =======球体=======
-    unsigned int sphereVBO, sphereEBO;
-    glGenVertexArrays(1, &sphereVAO);
-    glGenBuffers(1, &sphereVBO);
-    glGenBuffers(1, &sphereEBO);
-    
-    glBindVertexArray(sphereVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
-    glBufferData(GL_ARRAY_BUFFER, sphere_vertices.size()*sizeof(float),&sphere_vertices[0], GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphere_indices.size()*sizeof(float),&sphere_indices[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)(6 * sizeof(float)));
-    
-    // ===== 地板的顶点设置======
-    GLuint Fl_VBO;
-    glGenVertexArrays(1, &Fl_VAO);
-    glGenBuffers(1, &Fl_VBO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, Fl_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
-    glBindVertexArray(Fl_VAO);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
-    glBindVertexArray(0);
-    
-    // =======字体四边形的顶点设置======
-    glGenVertexArrays(1, &TextVAO);
-    glGenBuffers(1, &Text_VBO);
+    // 4. 纹理设置
+    setTextures();
 
-    glBindVertexArray(TextVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, Text_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (void*)0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    
-    // ===纹理加载=====
-    floorTextureID = loadTexture(texture_floor);
-    sunTextureID = loadTexture(texture_sun);
-    moonTextureID = loadTexture(texture_moon);
-    boxTextureID = loadTexture(texture_box);
-    
-    float initial_time = glfwGetTime();
-    deltaTime = lastTime = 0.0f;
+    // 5. 游戏循环渲染
+    initial_time = glfwGetTime();
     while (!glfwWindowShouldClose(window)) {
-        float current = glfwGetTime();
-        deltaTime = current - lastTime;
-        lastTime = current;
-        processInput(window); // 处理输入事件
-        
-        // 计算FPS
-        double currentTime = glfwGetTime();
-        frameCount++;
-        if ( currentTime - initial_time >= 1.0 ){
-            frame = frameCount;
-            frameCount = 0;
-            initial_time += 1;
-        }
-        
-        // 渲染指令
+        // 5.1. 处理输入事件
+        processInput(window);
+        calculateInLoop();
+        // 5.2 渲染指令
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        shadowShader.use();
-        shadowShader.setInt1("material.diffuse", 0);
-        shadowShader.setInt1("material.specular", 1.0f);
-        shadowShader.setFloat1("material.shininess", 64.0f);
-        shadowShader.setDirectionLight(lightPos, glm::vec3(0.3f), glm::vec3(0.5f), glm::vec3(1.0f));
-        shadowShader.setSpotLight(camera.camFront, camera.camPos, glm::cos(glm::radians(12.5f)), glm::cos(glm::radians(17.5f)), glm::vec3(0.2f), glm::vec3(0.5f), glm::vec3(1.0f));
+        // 5.3 渲染物体
+        renderScene(objShader);
         
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) window_width / (float) window_height, 0.1f, 100.0f);
-        glm::mat4 view = camera.getViewMatrix();
-        shadowShader.setMat4("projection", projection);
-        shadowShader.setMat4("view", view);
-        shadowShader.setVec3("viewPos", camera.camPos);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, floorTextureID);
-        renderScene(shadowShader);
-        
-        // 3. 渲染光源
+        // 5.4 渲染太阳(光源)
         renderLightSource(lampShader);
         
         // 4. 渲染字体
@@ -204,12 +116,12 @@ int main(int argc, const char * argv[]) {
         glfwSwapBuffers(window); // 颜色缓冲交换
         glfwPollEvents(); // 处理事件
     }
-    
+    // 6. 释放
     glDeleteVertexArrays(1, &cube_VAO);
     glDeleteVertexArrays(1, &lighterVAO);
     glDeleteVertexArrays(1, &Fl_VAO);
     glDeleteVertexArrays(1, &sphereVAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &cube_VBO);
     glDeleteBuffers(1, &sphereVBO);
     glDeleteBuffers(1, &sphereEBO);
     
@@ -229,7 +141,7 @@ void renderLightSource(Shader &shader){
     shader.setMat4("view", view);
     shader.setMat4("projection", projection);
     shader.setInt1("diffuseTexture", 0.0);
-    // 绘制光源
+    // 渲染光源
     glBindVertexArray(sphereVAO);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, sunTextureID);
@@ -237,6 +149,22 @@ void renderLightSource(Shader &shader){
 }
 
 void renderScene(Shader &shader){
+    // 设置着色器变量
+    shader.use();
+    shader.setInt1("material.diffuse", 0);
+    shader.setInt1("material.specular", 1.0f);
+    shader.setFloat1("material.shininess", 64.0f);
+    shader.setDirectionLight(lightPos, glm::vec3(0.3f), glm::vec3(0.5f), glm::vec3(1.0f));
+    shader.setSpotLight(camera.camFront, camera.camPos, glm::cos(glm::radians(12.5f)), glm::cos(glm::radians(17.5f)), glm::vec3(0.2f), glm::vec3(0.5f), glm::vec3(1.0f));
+    
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) window_width / (float) window_height, 0.1f, 100.0f);
+    glm::mat4 view = camera.getViewMatrix();
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+    shader.setVec3("viewPos", camera.camPos);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, floorTextureID);
+    
     // 渲染地板
     shader.use();
     glm::mat4 model = glm::mat4(1.0f);
@@ -256,7 +184,7 @@ void renderScene(Shader &shader){
     glDrawArrays(GL_TRIANGLES, 0, 36);
     glBindVertexArray(0);
     
-    // 绘制球体
+    // 渲染球体
     model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(0.8f, 0.0f, 1.3f));
     model = glm::scale(model, glm::vec3(sin(glfwGetTime())));
@@ -271,6 +199,8 @@ void renderScene(Shader &shader){
 void renderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color){
 
     shader.use();
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(window_width), 0.0f, static_cast<GLfloat>(window_height));
+    shader.setMat4("projection", projection);
     shader.setFloat3("textColor", color.x, color.y, color.z);
     glActiveTexture(GL_TEXTURE0);
     glBindVertexArray(TextVAO);
@@ -351,6 +281,94 @@ int init(){
     return 0;
 }
 
+void setVertices(){
+    // =======立方体=======
+    glGenVertexArrays(1, &cube_VAO);
+    glGenBuffers(1, &cube_VBO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, cube_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(cube_VAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*(sizeof(float)), (void*)(6 * sizeof(float)));
+    
+    // =======球体=======
+    // 球体
+    Sphere sphere(0.5f, 60, 60);
+    vector<float> sphere_vertices = sphere.getVertices();
+    sphere_indices = sphere.getIndices();
+    
+    glGenVertexArrays(1, &sphereVAO);
+    glGenBuffers(1, &sphereVBO);
+    glGenBuffers(1, &sphereEBO);
+    
+    glBindVertexArray(sphereVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, sphere_vertices.size()*sizeof(float),&sphere_vertices[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphere_indices.size()*sizeof(float),&sphere_indices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 6*(sizeof(float)), (void*)(6 * sizeof(float)));
+    
+    // ===== 地板的顶点设置======
+    glGenVertexArrays(1, &Fl_VAO);
+    glGenBuffers(1, &Fl_VBO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, Fl_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
+    glBindVertexArray(Fl_VAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
+    glBindVertexArray(0);
+    
+    // =======字体四边形的顶点设置======
+    glGenVertexArrays(1, &TextVAO);
+    glGenBuffers(1, &Text_VBO);
+    
+    glBindVertexArray(TextVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, Text_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void setTextures(){
+    // ===纹理加载=====
+    floorTextureID = loadTexture(texture_floor);
+    sunTextureID = loadTexture(texture_sun);
+    moonTextureID = loadTexture(texture_moon);
+    boxTextureID = loadTexture(texture_box);
+}
+
+void calculateInLoop(){
+    float current = glfwGetTime();
+    deltaTime = current - lastTime;
+    lastTime = current;
+    
+    // 计算FPS
+    double currentTime = glfwGetTime();
+    frameCount++;
+    if ( currentTime - initial_time >= 1.0 ){
+        frame = frameCount;
+        frameCount = 0;
+        initial_time += 1;
+    }
+}
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height){
     glViewport(0, 0, width, height);
 }
@@ -389,4 +407,3 @@ void mouseCallback(GLFWwindow* window, double x_pos, double y_pos){
 void scrollCallback(GLFWwindow *window, double x_offset, double y_offset){
     camera.processMouseScroll(y_offset);
 }
-
